e82a07ebc5511ee3680a1b68eeee85e621f10a2a
HeilagrFox <brazhnikova.03@bk.ru>
2026-03-14 19:22:58 +0300

    delete tmp docker install in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,23 +88,6 @@ resource "yandex_compute_instance" "vm" {
 
   metadata = {
     ssh-keys = "ubuntu:${var.ssh_public_key_content}"
-    user-data = <<-EOF
-      #cloud-config
-      package_update: true
-      packages:
-        - apt-transport-https
-        - ca-certificates
-        - curl
-        - gnupg
-        - lsb-release
-      runcmd:
-        - curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-        - echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-        - apt-get update
-        - apt-get install -y docker-ce docker-ce-cli containerd.io
-        - usermod -aG docker ubuntu
-        - sudo service docker start
-      EOF
   }
 }
 
